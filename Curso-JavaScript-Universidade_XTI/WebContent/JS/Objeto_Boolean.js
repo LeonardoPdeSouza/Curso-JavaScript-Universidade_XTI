215d09b547598ab8fdd5ba6283edadb61294c267
leona <leona@Leonardo>
2016-10-07 10:47:33 -0300

    Exemplo objeto_boolean

---
+++ Curso-JavaScript-Universidade_XTI/WebContent/JS/Objeto_Boolean.js
@@ -0,0 +1,28 @@
+var b = true;
+
+alert(b);
+
+alert(typeof(b));
+
+var bb = new Boolean(true);
+alert(typeof(bb));   
+alert(bb instanceof Boolean);    
+
+
+
+var b02 = new Boolean();  
+var b03 = new Boolean(1);  
+var b04 = new Boolean(""); 
+
+var b05 = new Boolean(null); 
+var b06 = new Boolean(0);
+alert(b06);
+
+alert(b06.valueOf());  
+
+alert(typeof(b06.valueOf()));  
+
+alert(typeof(b06.toString()));  
+
+alert(b6.toString()); 
+ 
