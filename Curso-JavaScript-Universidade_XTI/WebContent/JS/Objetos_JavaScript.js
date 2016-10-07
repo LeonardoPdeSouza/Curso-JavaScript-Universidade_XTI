a321512a0dbc53640cfe36ea59c371ebfab5e1b0
leona <leona@Leonardo>
2016-10-07 10:40:02 -0300

    Adicionando exemplo Objetos_JavaScript (html,js); alterando o titulo de tratamento_excecoes.html

---
+++ Curso-JavaScript-Universidade_XTI/WebContent/JS/Objetos_JavaScript.js
@@ -0,0 +1,12 @@
+var nome = "Leonardo";
+
+alert(typeof(nome));   
+
+alert(nome.length); 
+//new String(nome).length;
+
+alert(nome.toUpperCase());   
+
+var nome02 = new String("Leonardo");
+
+alert(nome02.length);
